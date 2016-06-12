047200ea4122367080c4bf1c62d8ebc1ff983c5a
Hao Fu <jamesfuhao@gmail.com>
2016-06-12 09:37:32 -0700

    PublicAPIField2

--- src/fu/hao/netsemantic/test/droidbench/BenchAndroidSpecific.java
+++ src/fu/hao/netsemantic/test/droidbench/BenchAndroidSpecific.java
@@ -98,5 +98,17 @@ public class BenchAndroidSpecific {
 		assertEquals(true, Results.results.contains(res));
 	}
 	
+	@Test
+	public void PublicAPIField2() {
+		args[0] = "C:/Users/hao/workspace/DroidBenchProj/PublicAPIField2/app/";
+		args[1] = "srcEventChains";
+		Main.main(args);
+		assertEquals(false, Results.results.isEmpty());
+		Map<String, String> res = new HashMap<>();
+		res.put("<android.telephony.SmsManager: void sendTextMessage(java.lang.String,java.lang.String,java.lang.String,android.app.PendingIntent,android.app.PendingIntent)>",
+				tm.getDeviceId());
+		assertEquals(true, Results.results.contains(res));
+	}
+	
 	
 }
